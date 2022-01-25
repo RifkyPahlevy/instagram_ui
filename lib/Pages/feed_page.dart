5b807bfb573f0dcf2cdfeb754530ed6560a4a5dc
RifkyPahlevy <rifkypahlevi514@gmail.com>
2022-01-25 11:29:17 +0700

    Penambahan fitur baru

--- lib/Pages/feed_page.dart
+++ lib/Pages/feed_page.dart
@@ -1,152 +1,146 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:instagram_ui/models/allusers.dart';
 
 class FeedPage extends StatelessWidget {
+  List<UsersModel> userAll = [];
+
+  Future getAllUser() async {
+    try {
+      var respone =
+          await http.get(Uri.parse('https://reqres.in/api/users?page=2'));
+      List data = (json.decode(respone.body) as Map<String, dynamic>)['data'];
+
+      data.forEach((element) {
+        userAll.add(UsersModel.fromJson(element));
+        print(userAll);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: Text(
-          'VGRAMAI',
-          style: TextStyle(fontStyle: FontStyle.italic, color: Colors.black),
-        ),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.movie_creation_sharp,
-                color: Colors.black,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.message_outlined,
-                color: Colors.black,
-              )),
-        ],
-      ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Stack(alignment: Alignment.center, children: [
-                  Container(
-                    height: 80,
-                    width: 80,
-                    margin: EdgeInsets.only(right: 10),
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [Colors.amber, Colors.red],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter),
-                        borderRadius: BorderRadius.circular(80 / 2)),
-                  ),
-                  Container(
-                    height: 70,
-                    width: 70,
-                    margin: EdgeInsets.only(right: 10),
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                'https://picsum.photos/id/${index + 237}/200/300')),
-                        color: Colors.blue,
-                        border: Border.all(width: 5, color: Colors.white),
-                        borderRadius: BorderRadius.circular(70 / 2)),
-                  ),
-                ]);
-              },
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Text(
+            'VGram',
+            style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
           ),
-          Expanded(child: ListView.builder(
-            itemBuilder: (context, index) {
-              return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Container(
-                    width: 200,
-                    height: 350,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.messenger_outlined,
+                  color: Colors.black,
+                ))
+          ],
+        ),
+        body: FutureBuilder(
+          future: getAllUser(),
+          builder: (context, snapshot) {
+            return ListView(children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                height: 100,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: userAll.length,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: [
+                        Stack(alignment: Alignment.center, children: [
+                          Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(40),
+                                gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomCenter,
+                                    colors: [Colors.red, Colors.amber])),
+                          ),
+                          Container(
+                            height: 77,
+                            width: 77,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(77 / 2),
+                                image: DecorationImage(
+                                    image: NetworkImage(userAll[index].avatar)),
+                                color: Colors.grey,
+                                border:
+                                    Border.all(color: Colors.white, width: 3)),
+                          )
+                        ])
+                      ],
+                    );
+                  },
+                ),
+              ),
+              ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  padding: EdgeInsets.all(20),
+                  itemCount: userAll.length,
+                  itemBuilder: (context, index) => Column(
                         children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                height: 50,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                    color: Colors.brown,
-                                    image: DecorationImage(
-                                        fit: BoxFit.cover,
-                                        image: NetworkImage(
-                                            'https://picsum.photos/id/${index + 240}/200/300')),
-                                    borderRadius:
-                                        BorderRadius.circular(50 / 2)),
-                              ),
-                              Expanded(
-                                child: ListTile(
-                                  title: Text(
-                                    'Username',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  trailing: IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(
-                                        Icons.more_vert_rounded,
-                                        color: Colors.black,
-                                      )),
-                                ),
-                              )
-                            ],
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Image.network(
-                            'https://picsum.photos/id/${index + 238}/200/300',
-                            fit: BoxFit.fitWidth,
-                            width: 450,
-                            height: 180,
-                          ),
-                          SizedBox(height: 20),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
+                          Container(
+                            child: Column(
                               children: [
-                                Icon(
-                                  Icons.houseboat_rounded,
-                                  color: Colors.black,
+                                ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundColor: Colors.grey[300],
+                                    backgroundImage:
+                                        NetworkImage(userAll[index].avatar),
+                                  ),
+                                  title: Text(userAll[index].firstName),
+                                  subtitle: Text(userAll[index].email),
+                                  trailing: Icon(Icons.more_vert_rounded),
                                 ),
-                                SizedBox(width: 20),
-                                Icon(
-                                  Icons.houseboat_rounded,
-                                  color: Colors.black,
+                                Image.network(
+                                  'https://picsum.photos/id/${index + 238}/200/300',
+                                  height: 300,
+                                  width: double.infinity,
+                                  fit: BoxFit.cover,
                                 ),
-                                SizedBox(width: 20),
-                                Icon(
-                                  Icons.houseboat_rounded,
-                                  color: Colors.black,
+                                Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Row(
+                                    children: [
+                                      IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(Icons.favorite_border)),
+                                      IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(Icons.chat)),
+                                      SizedBox(
+                                        width: 300,
+                                      ),
+                                      IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(Icons.bookmark_border)),
+                                    ],
+                                  ),
                                 ),
                               ],
                             ),
+                            height: 430.0,
+                          ),
+                          SizedBox(
+                            height: 10,
                           )
                         ],
-                      ),
-                    ),
-                    margin: EdgeInsets.only(bottom: 10),
-                  ));
-            },
-          ))
-        ],
-      ),
-    );
+                      )),
+            ]);
+          },
+        ));
   }
 }
---
+++ lib/models/allusers.dart
@@ -0,0 +1,42 @@
+// To parse this JSON data, do
+//
+//     final usersModel = usersModelFromJson(jsonString);
+
+import 'dart:convert';
+
+UsersModel usersModelFromJson(String str) =>
+    UsersModel.fromJson(json.decode(str));
+
+String usersModelToJson(UsersModel data) => json.encode(data.toJson());
+
+class UsersModel {
+  UsersModel({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
+
+  int id;
+  String email;
+  String firstName;
+  String lastName;
+  String avatar;
+
+  factory UsersModel.fromJson(Map<String, dynamic> json) => UsersModel(
+        id: json["id"],
+        email: json["email"],
+        firstName: json["first_name"],
+        lastName: json["last_name"],
+        avatar: json["avatar"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "email": email,
+        "first_name": firstName,
+        "last_name": lastName,
+        "avatar": avatar,
+      };
+}
